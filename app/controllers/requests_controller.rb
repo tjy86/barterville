2767ecf342e2db6ec21cd245ff4de94d6f0451c7
Raymond Chan <raycchan@gmail.com>
2012-11-27 18:41:00 -0500

    commit

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,9 +3,7 @@ class RequestsController < ApplicationController
     user = @authenticated_user
     requests = Request.where(:owner_id => @authenticated_user, :done => false)
     binding.pry
-    @reqs = {}
-    requests.each do |r|
-      Product.find(r.product_id)
-      r.
+    
+    
   end
 end
