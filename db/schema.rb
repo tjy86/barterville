33c74612b0abfe0a00d6c7a1cd33e69f9481cbc2
tjy86 <thomasjyang@gmail.com>
2012-11-27 10:31:50 -0500

    added email to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121126210411) do
+ActiveRecord::Schema.define(:version => 20121127152523) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20121126210411) do
     t.string   "photo"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "email"
   end
 
 end
