164336bea6fa05d3495303d0628b7ec700940a4e
tjy86 <thomasjyang@gmail.com>
2012-11-27 18:02:41 -0500

    barter page done

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,11 +1,6 @@
 class RequestsController < ApplicationController
   def index
     user = @authenticated_user
-    requests = Request.where(:owner_id => @authenticated_user, :done => false)
-    binding.pry
-    @reqs = {}
-    requests.each do |r|
-      Product.find(r.product_id)
-      r.
+    @requests = Request.where(:owner_id => @authenticated_user, :done => false)
   end
 end
