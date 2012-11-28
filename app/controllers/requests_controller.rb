fff05cc8587e1d48c7e7e3a7be30489ce6247a96
tjy86 <thomasjyang@gmail.com>
2012-11-28 11:56:54 -0500

    styling and bug fixes'

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,8 @@
 class RequestsController < ApplicationController
   def index
+
     user = @authenticated_user
-    requests = Request.where(:owner_id => @authenticated_user, :done => false)
+    @requests = Request.where(:owner_id => @authenticated_user, :done => false)
 
   end
 end
